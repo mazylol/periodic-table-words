fe75e62e17dcccc9f1fec014633bfd0758abfff5
Mazylol <landonp95@gmail.com>
2023-01-04 08:28:58 -0500

    getting better but still incredibly stupid

--- src/main/java/com/mazylol/PeriodicTableWords/Main.java
+++ src/main/java/com/mazylol/PeriodicTableWords/Main.java
@@ -3,27 +3,25 @@ package com.mazylol.PeriodicTableWords;
 import com.mazylol.PeriodicTableWords.Utils.Files;
 import com.mazylol.PeriodicTableWords.Utils.Numbers;
 
-import java.util.List;
+import java.io.FileNotFoundException;
 
 public class Main {
-    public static void main(String[] args) {
-        List<String> words = Files.GetArray("words_alpha.txt");
-        List<String> elements = Files.GetArray("elements.txt");
+    public static void main(String[] args) throws FileNotFoundException {
+        String[] words = Files.GetWords();
+        String[] elements = Files.GetElements();
 
-        assert words != null;
-        assert elements != null;
+        for (int i = 0; i < 10000; i++) {
+            String w1 = elements[Numbers.getRandomNumber(0, 118)];
+            String w2 = elements[Numbers.getRandomNumber(0, 118)];
+            String w3 = elements[Numbers.getRandomNumber(0, 118)];
+            String w4 = elements[Numbers.getRandomNumber(0, 118)];
 
-        for (int i = 0; i < 1000; i++) {
-            int n1 = Numbers.getRandomNumber(0, 118);
-            int n2 = Numbers.getRandomNumber(0, 118);
-            int n3 = Numbers.getRandomNumber(0, 118);
-            int n4 = Numbers.getRandomNumber(0, 118);
+            String word = w1 + w2 + w3 + w4;
 
-            String word = elements.get(n1) + elements.get(n2) + elements.get(n3) + elements.get(n4);
-
-            for (int x = 0; x < words.size(); x++) {
-                if (word.toLowerCase() == words.get(x)) {
+            for (String s : words) {
+                if (word.toLowerCase().equals(s)) {
                     System.out.println(word.toLowerCase());
+                    System.out.printf("(%s %s %s %s)\n", w1, w2, w3, w4);
                 }
             }
         }
--- src/main/java/com/mazylol/PeriodicTableWords/Utils/Files.java
+++ src/main/java/com/mazylol/PeriodicTableWords/Utils/Files.java
@@ -2,29 +2,38 @@ package com.mazylol.PeriodicTableWords.Utils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Files {
+    public static String[] GetWords() throws FileNotFoundException {
+        String[] words = new String[370105];
 
-    public static List<String> GetArray(String path) {
-        try {
-            Scanner scanner = new Scanner(new File(path));
+        Scanner scanner = new Scanner(new File("words_alpha.txt"));
 
-            List<String> list = new ArrayList<>();
-
-            while (scanner.hasNextLine()) {
-                list.add(scanner.nextLine());
-            }
-
-            scanner.close();
-
-            return list;
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-
-            return null;
+        int i = 0;
+        while (scanner.hasNextLine()) {
+            words[i] = scanner.nextLine();
+            i++;
         }
+
+        scanner.close();
+
+        return words;
+    }
+
+    public static String[] GetElements() throws FileNotFoundException {
+        String[] elements = new String[118];
+
+        Scanner scanner = new Scanner(new File("elements.txt"));
+
+        int i = 0;
+        while (scanner.hasNextLine()) {
+            elements[i] = scanner.nextLine();
+            i++;
+        }
+
+        scanner.close();
+
+        return elements;
     }
 }
